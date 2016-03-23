24e61860bc0470b1b1152ccb0f14d1833ba8ec84
xukw <honest_kw@126.com>
2016-03-23 19:05:48 +0800

    陈宏修改后的

--- UI_Button_Test/ViewController.m
+++ UI_Button_Test/ViewController.m
@@ -21,7 +21,7 @@
     [self.view addSubview:lb];
     lb.frame = CGRectMake(40, 20, 100, 20);
     lb.text = @"按钮1";
-    lb.textColor = [UIColor blackColor];
+    lb.textColor = [UIColor lightGrayColor];
     UIButton *bt1 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [self.view addSubview:bt1];
     bt1.frame = CGRectMake(40, 40, 100, 68);
@@ -40,26 +40,53 @@
     //UIControlStateApplication
     //保留状态  UIControlStateStateReserved
     
+    UILabel *lb2 = [[UILabel alloc] init];
+    [self.view addSubview:lb2];
+    lb2.frame = CGRectMake(40, 120, 100, 20);
+    lb2.text = @"按钮2（多次点击）";
+    lb2.textColor = [UIColor blueColor];
     UIButton *bt2 = [UIButton buttonWithType:UIButtonTypeCustom];
     [self.view addSubview:bt2];
     bt2.frame=CGRectMake(40, 140, 100,68);
     [bt2 setTitle:@"这是双击事件" forState:UIControlStateNormal];
-    [bt2 setImage:[UIImage imageNamed:@"3.jpeg"] forState:UIControlStateNormal];
+    [bt2 setBackgroundImage:[UIImage imageNamed:@"3.jpeg"] forState:UIControlStateNormal];
 //    bt2.backgroundColor = [UIColor blackColor];
     //双击事件
     [bt2 addTarget:self action:@selector(exchange) forControlEvents:UIControlEventTouchDownRepeat];
     
     
+    UILabel *lb3 = [[UILabel alloc] init];
+    [self.view addSubview:lb3];
+    lb3.frame = CGRectMake(40, 220, 100, 20);
+    lb3.text = @"按钮3（键内松开）";
     UIButton *bt3 = [UIButton buttonWithType:UIButtonTypeCustom];
     [self.view addSubview:bt3];
     [bt3 setTitle:@"这是个键内松开触发事件按钮"  forState:UIControlStateNormal];
     [bt3 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     bt3.frame=CGRectMake(40,240, 100, 68);
-    [bt3 setImage:[UIImage imageNamed:@"4.jpg"] forState:UIControlStateNormal];
+    [bt3 setBackgroundImage:[UIImage imageNamed:@"4.jpg"] forState:UIControlStateNormal];
     bt3.backgroundColor = [UIColor greenColor];
 //    bt3.alpha = 0.6;
     //在按钮内松开
     [bt3 addTarget:self action:@selector(upInside) forControlEvents:UIControlEventTouchUpInside];
+    
+    UILabel *lb4 = [[UILabel alloc] init];
+    [self.view addSubview:lb4];
+    lb4.frame = CGRectMake(40, 320, 100, 20);
+    lb4.text = @"按钮4";
+    lb4.tintColor = [UIColor blackColor];
+    
+    //
+    UIButton *bt4 = [[UIButton alloc] init];
+    bt4 = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.view addSubview:bt4];
+    bt4.frame = CGRectMake(40, 340,180,300);
+    [bt4 setBackgroundImage:[UIImage imageNamed:@"/Users/qingyun/Desktop/百度云同步盘/xkw/Code/322/UI_Button_Test/UI_Button_Test/me.JPG"] forState:UIControlStateNormal];
+    bt4.backgroundColor = [UIColor grayColor];
+    [bt4 setTitle:@"陈宏" forState:UIControlStateNormal];
+    [bt4 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    //设置按钮图片距离边框的距离（上，左，下，右）
+    bt4.contentEdgeInsets = UIEdgeInsetsMake(10, 8, 10,8);
 }
 
 - (void)didReceiveMemoryWarning {
@@ -68,8 +95,10 @@
 }
 - (void)exchange{
     NSLog(@"陈宏，，，，，");
+    NSLog(@"当前类的类名: %s",__func__);
 }
 - (void)upInside{
     NSLog(@"在按钮内松开");
+    NSLog(@"当前类的类名为：%s",__func__);
 }
 @end
